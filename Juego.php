aee7f0db8d7ac7d6b3297ce2247c486de5ec968a
josejimenezgallego <31858531+josejimenezgallego@users.noreply.github.com>
2019-12-01 21:28:52 +0100

    Add files via upload

---
+++ Juego.php
@@ -0,0 +1,80 @@
+<?php
+namespace juego;
+
+use Jugador, Carta;
+
+class Juego 
+{
+   private $baraja;
+   private $usuario;
+
+   public function __construct($nombre)
+   {      
+      $usuario=new Jugador($nombre);       
+     
+      $palos = array('oros','copas','espadas','bastos');
+      $numeros = array("1","2","3","4","5","6","7","10","11","12");
+      
+      foreach ($palos as $palo) 
+      {
+        foreach ($numeros as $numero) 
+        {
+           //$baraja[] = array("numero"=>$numero, "palo"=>$palo);
+           $baraja[] = new Carta($palo, $numero);
+        }
+      }
+    }
+
+
+
+    // BARAJAR: DESORDENAR LAS CARTAS
+    public function iniciaJuego()   
+    {
+         shuffle($this->baraja);            
+    }
+
+    // PASA LA CARTA SUPERIOR A LA BARAJA DEL JUGUADOR
+    public function pideCarta()   
+    {
+        if (!$usuario->compruebaPasa())
+        {
+           $carta_superior=reset($baraja);   // extrae la primera (superior) carta del mazo
+           $usuario->jugada=array_push($carta_superior);    // añadir la carta a baraja jugador
+           array_shift($baraja);    // elimina primera carta del mazo de cartas
+        }
+    }
+
+    // ASIGNA ARRAY DE CARTAS AL USUARIO (solo para pruebas)
+    public function debugBaraja($cartasPrueba)
+    {
+         $this->baraja=$cartasPrueba;         
+    }
+
+    // GUARDA JUGADOR
+    public function setJugador($jugador)   
+    {
+         $this->jugador=$jugador;         
+    }
+
+    // DEVUELVE JUGADOR
+    public function getJugador()   
+    {       
+      return $this->usuario;
+    }
+
+    // GUARDA BARAJA
+    public function setJugador($baraja)   
+    {
+         $this->baraja=$baraja;         
+    }
+
+    // DEVUELVE JUGADOR
+    public function getBaraja()   
+    {       
+      return $this->baraja;
+    }    
+
+ }
+
+
+ ?>
---
+++ Jugador.php
@@ -0,0 +1,61 @@
+<?php
+namespace juego;
+
+class Jugador
+{
+   // ATRIBUTOS
+   private $nombre="";
+   private $jugada="";
+
+   private function __construct($nombre)
+   {      
+      $this->nombre=$nombre;
+      $this->jugada=array();
+   }
+
+    // DEVUELBE TRUE SI LA SUMA DE LAS CARTAS > 7.5
+    public function compruebaPasa()   
+    {
+      if ($this->compruebaJugada()>7.5)
+         return true;
+         else
+         return false; 
+    }
+    
+    // DEVUELBE ACTUAL PUNTUACIÓN
+    public function compruebaJugada()   
+    {
+        $puntos=0;
+
+        foreach($this->jugada as $carta)
+          {
+            $puntos+=$carta->getValorCarta();              
+          }
+        return $puntos;
+       
+    }
+
+    // GUARDA NOMBRE USUARIO
+    public function setNombreUsuario($nombre)   
+    {
+         $this->nombre=$nombre;         
+    }
+
+    // DEVUELVE NOMBRE USUARIO
+    public function getNombreUsuario()   
+    {
+        return $this->nombre;
+    }
+    // GUARDA JUGADA
+    public function setJugadaUsuario($jugada)   
+    {
+         $this->jugada=$jugada;         
+    }
+
+    // DEVUELVE JUGADA
+    public function getJugadaUsuario()   
+    {
+        return $this->jugada;
+    }
+ }
+?>
